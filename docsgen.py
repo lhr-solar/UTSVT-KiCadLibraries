2d89e2d2263134fca2808ca2521ec528ee919bad
Lakshay Gupta <lakshay45gupta@gmail.com>
2024-01-03 20:39:55 -0600

    better formatting

--- docsgen.py
+++ docsgen.py
@@ -2,7 +2,7 @@ import os
 
 
 def exportPDF():
-    outputDir = (projectDir + "/"+ projectName [:-10] + ".pdf")
+    outputDir = (projectDir + "/"+ projectName [:-10] + "Schematic.pdf")
     schematicDir = (projectDir + "/" + schematicName)
     os.system(f"kicad-cli sch export pdf -o {outputDir} {schematicDir}")
 
@@ -11,11 +11,10 @@ def exportStep():
     pcbDir = (projectDir + "/" + pcbName)
     os.system(f"kicad-cli pcb export step --force --subst-models -o {outputDir} {pcbDir}")
 
-def exportFrontBack():
+def exportPCB():
     outputDir = (projectDir + "/"+ projectName [:-10] + "PCB.svg")
     pcbDir = (projectDir + "/" + pcbName)
     os.system(f"kicad-cli pcb export svg --page-size-mode 2 --exclude-drawing-sheet --layers Edge.Cuts,F.SilkS,F.Cu,B.SilkS,B.Cu -o {outputDir} {pcbDir}")
-    #os.system(f"kicad-cli pcb export svg --page-size-mode 2 --exclude-drawing-sheet --mirror --layers Edge.Cuts,B.SilkS,B.Cu -o {k7_proj.proj_dir}docs/2D/{k7_proj.proj_name}_back.svg {k7_proj.pcb_file}")
 
 schematicName = "";
 pcbName = ""
@@ -33,4 +32,4 @@ for i in os.listdir():
 os.chdir(cliPath)
 exportPDF();
 exportStep();
-exportFrontBack()
+exportPCB()
