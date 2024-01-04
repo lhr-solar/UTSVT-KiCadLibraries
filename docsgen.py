b73a546a68ec32ce399d75bbcffb4ebc596b97fb
Lakshay Gupta <lakshay45gupta@gmail.com>
2024-01-03 20:19:17 -0600

    added STEP and PCB export

--- docsgen.py
+++ docsgen.py
@@ -1,4 +1,3 @@
-# .\kicad-cli.exe sch export pdf --output C:\Users\laksh\Documents\LHR\BPS-AmperesPCB\BPS-Amperes.pdf C:\Users\laksh\Documents\LHR\BPS-AmperesPCB\BPS-Amperes.kicad_sch
 import os
 
 
@@ -7,7 +6,16 @@ def exportPDF():
     schematicDir = (projectDir + "/" + schematicName)
     os.system(f"kicad-cli sch export pdf -o {outputDir} {schematicDir}")
 
+def exportStep():
+    outputDir = (projectDir + "/"+ projectName [:-10] + ".step")
+    pcbDir = (projectDir + "/" + pcbName)
+    os.system(f"kicad-cli pcb export step --force --subst-models -o {outputDir} {pcbDir}")
 
+def exportFrontBack():
+    outputDir = (projectDir + "/"+ projectName [:-10] + "PCB.svg")
+    pcbDir = (projectDir + "/" + pcbName)
+    os.system(f"kicad-cli pcb export svg --page-size-mode 2 --exclude-drawing-sheet --layers Edge.Cuts,F.SilkS,F.Cu,B.SilkS,B.Cu -o {outputDir} {pcbDir}")
+    #os.system(f"kicad-cli pcb export svg --page-size-mode 2 --exclude-drawing-sheet --mirror --layers Edge.Cuts,B.SilkS,B.Cu -o {k7_proj.proj_dir}docs/2D/{k7_proj.proj_name}_back.svg {k7_proj.pcb_file}")
 
 schematicName = "";
 pcbName = ""
@@ -24,3 +32,5 @@ for i in os.listdir():
          projectName = i
 os.chdir(cliPath)
 exportPDF();
+exportStep();
+exportFrontBack()
