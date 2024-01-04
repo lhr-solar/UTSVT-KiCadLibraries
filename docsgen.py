902e64f9e48964afd4ca2ac056b5ed7e7b4c9e6a
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2024-01-03 21:14:32 -0600

    added to do

--- docsgen.py
+++ docsgen.py
@@ -1,11 +1,12 @@
 import os
 
-
+#To do: Verify that it works with multiline schematics
 def exportPDF():
     outputDir = (projectDir + "/"+ projectName [:-10] + "Schematic.pdf") # Sets the directory of the output (overwrites the file or creates it)
     schematicDir = (projectDir + "/" + schematicName) # Directory for schematic being used to create the PDF
     os.system(f"kicad-cli sch export pdf -o {outputDir} {schematicDir}")
 
+#To do: Make it export relative to board origin
 def exportStep():
     outputDir = (projectDir + "/"+ projectName [:-10] + ".step")
     pcbDir = (projectDir + "/" + pcbName) # Directory for the pcb being used for step
@@ -16,11 +17,12 @@ def exportPCB():
     pcbDir = (projectDir + "/" + pcbName) # Directory for the pcb being used for SVG
     os.system(f"kicad-cli pcb export svg --page-size-mode 2 --exclude-drawing-sheet --layers Edge.Cuts,F.SilkS,F.Cu,B.SilkS,B.Cu -o {outputDir} {pcbDir}")
 
-schematicName = "";
+schematicName = ""
 pcbName = ""
 projectName = ""
 projectDir = os.path.dirname(os.getcwd()) # Gets the parents directory since directory is currently in UTSVT-KiCAD
 os.chdir(projectDir) # Changes to parent directory
+#To do: Change depending on linux or windows
 cliPath = r'C:/Program Files/KiCad/7.0/bin' # Path of the KiCAD-cli binary. Gonna need to change for linux and Mac
 for i in os.listdir():
     if (i.endswith(".kicad_sch")):
